e52b19f6f13943b5595ca5f367cb57f6b0727461
yuzinnee <yuzinnee@naver.com>
2022-10-14 11:23:35 +0900

    Modify : test.js 오류 수정

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 // 1부터 50의 자연수 중 짝수를 구하는 함수
 let arr=[]
-function isEven(i) {
+function isEven() {
   for (let i = 1; i < 51; i++) {
     if (i % 2 === 0) {
     arr.push(i)
-    } else if {
-      return 0;
-    }
   }
-} isEven(i)
+  return arr
+}
+isEven();
